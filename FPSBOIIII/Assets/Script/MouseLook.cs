46d3b31a0114a362f2738a7764b2329723114724
BUG2000 <drorafl@gmail.com>
2020-09-25 12:04:33 +0300

    Player movement (with camera movement) #first update
    Player movement (with camera movement) first update

---
+++ FPSBOIIII/Assets/Script/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float mouseSentivityX = 0f;
+    [SerializeField] float mouseSentivityY = 0f;
+    public Transform playerBody;
+    float xRotation = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+       Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSentivityX * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSentivityY * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
