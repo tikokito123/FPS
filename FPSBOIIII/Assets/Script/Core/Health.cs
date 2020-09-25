fb1ed73f60117f25e8fd6e6890d17e3d6914c6d9
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-09-25 22:37:52 +0300

    Added a weapon script :)

---
+++ FPSBOIIII/Assets/Script/Core/Health.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
---
+++ FPSBOIIII/Assets/Script/Weapon/Weapon.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+
+public class Weapon : MonoBehaviour
+{
+    //gun states!
+    public int damage;
+    public float timeBetweenShots, spread, range, reloadTime, timeBetweenShooting;
+    public int magazineSize, bulletsPerTap;
+    public bool allowButtonHold;
+    int bulletsLeft, bulletsShot;
+
+
+    //bools
+    bool shooting, readyToShoot, reloading;
+
+    //refernces
+    public Camera fpsCam;
+    public RaycastHit rayHit;
+    public LayerMask whatIsEnemy;
+    private void Awake()
+    {
+        bulletsLeft = magazineSize;
+        readyToShoot = true;
+    }
+    private void Update()
+    {
+        PlayerInput();
+    }
+    private void PlayerInput()
+    {
+        if (allowButtonHold) shooting = Input.GetKey(KeyCode.Mouse0);
+        else shooting = Input.GetKeyDown(KeyCode.Mouse0);
+        if (Input.GetKeyDown(KeyCode.R) && bulletsLeft < magazineSize && !reloading) Reload();
+        //shoot
+        if (readyToShoot && shooting && !reloading && bulletsLeft > 0)
+        {
+            bulletsShot = bulletsPerTap;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        readyToShoot = false;
+        //spread!
+        float x = UnityEngine.Random.Range(-spread, spread);
+        float y = UnityEngine.Random.Range(-spread, spread);
+        //calculate Directions with spread
+        Vector3 direction = fpsCam.transform.forward + new Vector3(x,y,0);
+        //raycast!
+        var hit = Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out rayHit, range, whatIsEnemy);
+        if (hit)
+        {
+            Debug.Log(rayHit.collider.name);
+            if (rayHit.collider.CompareTag("Enemy"))
+            {
+                rayHit.collider.GetComponent<Health>().TakeDamage(damage);
+            }
+        }
+        bulletsLeft--;
+        bulletsShot--;
+        Invoke("ResetShot", timeBetweenShooting);
+        if (bulletsShot > 0 && bulletsLeft > 0)
+        {
+            Invoke("Shoot", timeBetweenShooting);
+        }
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+    }
+
+    private void Reload()
+    {
+        reloading = true;
+        Invoke("ReloadingFinish", reloadTime);
+    }
+    private void ReloadingFinish()
+    {
+        bulletsLeft = magazineSize;
+        reloading = false;
+    }
+}
