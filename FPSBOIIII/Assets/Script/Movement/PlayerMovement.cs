12135755fe1bbce7faf142d3fbf6cd3138bd10cf
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-09-28 08:31:59 +0300

    improv terrain!

--- FPSBOIIII/Assets/Script/Movement/PlayerMovement.cs
+++ FPSBOIIII/Assets/Script/Movement/PlayerMovement.cs
@@ -36,6 +36,5 @@ public class PlayerMovement : MonoBehaviour
         controller.Move(move * speed * Time.deltaTime);
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
-        print(velocity.y);
     }
 }
