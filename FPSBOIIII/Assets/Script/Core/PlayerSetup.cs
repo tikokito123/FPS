94bac89cd2f9cdbc25fca0507cd43d28400defc5
BUG2000 <drorafl@gmail.com>
2020-09-30 04:29:51 +0300

    Multiplayer server work #1

---
+++ FPSBOIIII/Assets/Script/Core/PlayerSetup.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Mirror;
+
+
+public class PlayerSetup : NetworkBehaviour 
+{
+    Camera sceneCamera;
+    [SerializeField] Behaviour[] componentToDisable;
+    void Start()
+    {
+        if (!isLocalPlayer)
+        {
+            for (int i = 0; i < componentToDisable.Length; i++)
+            {
+                componentToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCamera = Camera.main;
+            if (sceneCamera != null)
+            {
+                sceneCamera.gameObject.SetActive(false);
+
+            }
+        }
+    }
+    void OnDisable()
+    {
+        if (sceneCamera != null)
+        {
+            sceneCamera.gameObject.SetActive(true);
+
+        }
+    }
+}
