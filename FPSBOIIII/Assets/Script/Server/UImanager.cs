407f037ba9361b64913ea31d27dba8ee9e4e0d8d
tikokito123 <30938070+tikokito123@users.noreply.github.com>
2020-10-01 09:39:42 +0300

    Server step 1

---
+++ FPSBOIIII/Assets/Script/Server/UImanager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UImanager : MonoBehaviour
+{
+    public static UImanager instance;
+    public GameObject startMenu;
+    public InputField userNameField;
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance Already exist, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    public void ConnectedToServer()
+    {
+        startMenu.SetActive(false);
+        userNameField.interactable = false;
+        Client.instance.ConnectedToServer();
+    }
+}
